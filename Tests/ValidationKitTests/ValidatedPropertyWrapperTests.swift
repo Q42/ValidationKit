304fc31f0da24ddd562fc7edae88c26140d3f617
Mathijs Bernson <mathijsb@q42.nl>
2021-11-26 15:02:02 +0100

    Update ValidatedPropertyWrapperTests.swift

--- Tests/ValidationKitTests/ValidatedPropertyWrapperTests.swift
+++ Tests/ValidationKitTests/ValidatedPropertyWrapperTests.swift
@@ -1,7 +1,7 @@
 @testable import ValidationKit
 import XCTest
 
-class ValidatedTests: XCTestCase {
+class ValidatedPropertyWrapperTests: XCTestCase {
   struct Person {
     @Validated(.notEmpty)
     var name: String? = nil
@@ -13,10 +13,14 @@ class ValidatedTests: XCTestCase {
   func testValidatable() {
     let validPerson = Person(name: "Mathijs", postalCode: "8049BW")
     XCTAssertTrue(validPerson.$name.isValid)
+    XCTAssertEqual(validPerson.$name.value, "Mathijs")
     XCTAssertTrue(validPerson.$postalCode.isValid)
+    XCTAssertEqual(validPerson.$postalCode.value, "8049BW")
 
     let invalidPerson = Person(name: "", postalCode: "1234")
     XCTAssertFalse(invalidPerson.$name.isValid)
+    XCTAssertNil(invalidPerson.$name.value)
     XCTAssertFalse(invalidPerson.$postalCode.isValid)
+    XCTAssertNil(invalidPerson.$postalCode.value)
   }
 }
